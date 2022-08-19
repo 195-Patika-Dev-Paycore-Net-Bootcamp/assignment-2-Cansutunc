f2621ac9e2551f293a16ef26a0c44006d6677adc
Cansu Tunç <cansutunc00@gmail.com>
2022-08-20 01:49:22 +0300

    Add project files.

---
+++ ModelValidationProject/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims; 
+
+namespace ModelValidationProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static List<Staff> StaffList = new List<Staff>() //creating the static list for staffs
+        {
+            new Staff   //member of list as instance of class
+            {
+                    Id =1,
+                    Name = "Deny",
+                    Lastname = "Sellen",
+                    DateOfBirth = new System.DateTime(1989,01,01),
+                    Email="deny@gmail.com",
+                    PhoneNumber ="+90555443366",
+                    Salary = 4450
+            },
+            new Staff   // another member of list as instance of class
+            {
+                    Id =2,
+                    Name = "Chiara",
+                    Lastname = "Leo",
+                    DateOfBirth = new System.DateTime(2000,01,01),
+                    Email="chiara@gmail.com",
+                    PhoneNumber ="+905409993330",
+                    Salary = 4000
+            }
+
+        };
+        [HttpGet]
+        public List<Staff> GetUsers()
+        {
+            var users = StaffList.OrderBy(x => x.Id).ToList<Staff>(); //validation 
+            return StaffList;
+        }
+
+        [HttpGet("{id}")]
+
+        public Staff GetById(int id)                                //validation
+        {
+
+            var user = StaffList.Where(Staff => Staff.Id == id).SingleOrDefault();
+            return user;
+        }
+
+        [HttpPost]
+        public IActionResult AddStaff([FromBody] Staff newStaff)    //validation
+        {
+            var staff = StaffList.SingleOrDefault(x => x.Name == new Staff().Name);//checking the existing Staff before creating
+
+            if (staff is not null)
+            {
+                return BadRequest();
+            }
+            StaffList.Add(newStaff); //if staff is null then add the newStaff to Stafflist
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStaff(int id, [FromBody] Staff updatedStaff) //update method and updates every field of the member.
+        {
+            var  staff= StaffList.SingleOrDefault(x => x.Id == id);
+           
+            if(staff is null)
+            {
+                return BadRequest();
+            }
+            
+            staff.Name = updatedStaff.Name != default ? updatedStaff.Name : updatedStaff.Name;
+            staff.Lastname= updatedStaff.Lastname!= default ? updatedStaff.Lastname : updatedStaff.Lastname;
+            staff.PhoneNumber=updatedStaff.PhoneNumber!=default ? updatedStaff.PhoneNumber : updatedStaff.PhoneNumber;
+            staff.DateOfBirth=updatedStaff.DateOfBirth!= default ? updatedStaff.DateOfBirth : updatedStaff.DateOfBirth;
+            staff.Email=updatedStaff.Email!=default ? updatedStaff.Email : updatedStaff.Email;
+            staff.Salary=updatedStaff.Salary!=default ? updatedStaff.Salary : updatedStaff.Salary;
+            staff.Id=updatedStaff.Id!=default ? updatedStaff.Id : updatedStaff.Id;
+
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeletedStaff(int id)
+        {
+            
+            var staff=StaffList.SingleOrDefault(x => x.Id == id);//validation for checking if there is same id in list
+            if (staff == null)
+            {
+                return BadRequest();
+            }
+            StaffList.Remove(staff);// if its null, return badrequest, if not remove the item with Remove Method.
+            return Ok();
+        }
+
+    }
+}
---
+++ ModelValidationProject/Staff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationProject
+{
+    public class Staff
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(120, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 20)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(120, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 20)]
+        public string Lastname { get; set; }
+
+        [Required]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Please enter correct email without specific characters except dot")]
+        public string Email { get; set; }
+
+        [Required]
+        [Range(typeof(DateTime), "11/11/1945", "10/10/2002", ErrorMessage = "Value for {0} must be between {1} and {2}")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(\+[0-9]{12})$", ErrorMessage = "Please enter correct phone number with country specific number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Range(2000, 9000, ErrorMessage = "Salary Must be between 2000 to 9000")]
+        public int Salary { get; set; }
+
+    }
+}
